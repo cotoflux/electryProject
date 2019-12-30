704163c58aacc2514daaca22f2fd0276360a4e9e
cotoflux <guaupelu@hotmail.com>
2019-12-30 21:58:14 +0100

    Cambiada ruta para que el usuario no vea la ruta si no esta logeado y que vuelva al inicio si no.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Tus Posts</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +14,10 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Bienvenid@ {{ Auth::user()->name }}, esta es la selección de posts que hemos hecho para ti!
 
-
+                    <div class="col align-self-center-card"><a href={{"/json-api"}}>Todos los posts</a></div>
+                    
                 </div>
             </div>
         </div>
--- resources/views/someviewglobal.blade.php
+++ resources/views/someviewglobal.blade.php
@@ -10,18 +10,21 @@
         <div>
 
         <table border=1 class="card flex-center container-fluid">
-            <tr><th>UserId</th>
-            <th>Id Entrada</th>
-            <th>Titulo de la Entrada</th>
-            <th>Mensaje del Post</th></tr>
-        @foreach($posts as $post)
-            <tr>
-            <td style="padding-left:15px; padding-right: 15px">{{ $post->userId}}</td>
-            <td style="padding-left:15px; padding-right: 15px"><a href="/json-api/{{$post->id}}">{{ $post->id}}</a></td>
-            <td style="padding-left:15px; padding-right: 15px"><a href="/json-api/{{$post->id}}">{{ $post->title}}</a></td>
-            <td style="padding-left:15px; padding-right: 15px">{{ $post->body}}</td>
-        </tr>
-        @endforeach
+                <tr>
+                    <th>UserId</th>
+                    <th>Id Entrada</th>
+                    <th>Titulo de la Entrada</th>
+                    <th>Mensaje del Post</th>
+                </tr>
+            @foreach($posts as $post)
+                <tr>
+                    <td style="padding-left:15px; padding-right: 15px">{{ $post->userId}}</td>
+                    <td style="padding-left:15px; padding-right: 15px"><a href="/json-api/{{$post->id}}">{{ $post->id}}</a></td>
+                    <td style="padding-left:15px; padding-right: 15px"><a href="/json-api/{{$post->id}}">{{ $post->title}}</a></td>
+                    <td style="padding-left:15px; padding-right: 15px">{{ $post->body}}</td>
+                </tr>
+            @endforeach
+            
         </table>
 
         
